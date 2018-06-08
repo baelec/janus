3a49c76d6f94508d0d3e699fc8f49025c2841faf
Michael Lynch <mike@ravencrest.io>
2018-06-07 23:48:38 -0400

    start script

--- scripts/start.js
+++ scripts/start.js
@@ -1,1 +1,18 @@
-const child = spawn('electron', ['', '-type', 'f']);
+const { spawn, exec } = require('child_process');
+const path = require('path');
+const webpack = require('webpack');
+const config = require('../config/webpack.config.dev');
+
+const compiler = webpack(config);
+
+const watching = compiler.watch({
+  aggregateTimeout: 300,
+  poll: undefined
+}, (err, stats) => {
+  const electron = path.resolve('./node_modules/.bin/electron');
+  const main = path.resolve('./build/webpack/main.js');
+  const child = spawn(electron, [main]);
+  child.on('close', (code, signal) => {
+      watching.close();
+    });
+  });
